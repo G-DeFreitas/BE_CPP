a34424a2e61a8d036537b56c819bc64176dddd64
Guillaume De Freitas <81011333+G-DeFreitas@users.noreply.github.com>
2022-03-30 17:04:58 +0200

    Update main.cpp

--- BE_CPP/src/main.cpp
+++ BE_CPP/src/main.cpp
@@ -1,17 +1,24 @@
 #include <Arduino.h>
+#define SENSOR_PIN 6
 void setup()
 {
+
   // put your setup code here, to run once:
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(LED_BUILTIN_AUX, OUTPUT);
+  pinMode(SENSOR_PIN, INPUT);
+  Serial.begin(9600);
+  //
 }
+int temp;
 
 void loop()
 {
+  temp = analogRead(SENSOR_PIN);
+  Serial.println("test");
   // put your main code here, to run repeatedly:
-  digitalWrite(LED_BUILTIN_AUX, HIGH);
+  digitalWrite(LED_BUILTIN, HIGH);
   delay(1000);
-  digitalWrite(LED_BUILTIN_AUX, LOW);
+  digitalWrite(LED_BUILTIN, LOW);
   delay(1000);
 
 }
