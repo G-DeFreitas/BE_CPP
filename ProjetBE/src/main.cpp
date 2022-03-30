4ee48bb63a1c32df46760af57e2bbef9529c089c
I-Badier <isaure.badier@gmail.com>
2022-03-30 18:00:45 +0200

    Temp_sensor OK

--- ProjetBE/src/main.cpp
+++ ProjetBE/src/main.cpp
@@ -1,11 +1,27 @@
 #include <Arduino.h>
 
+#define TEMP_SENSOR_PIN A0
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600,SERIAL_8N1);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(TEMP_SENSOR_PIN, INPUT);
 }
+
+int temp;
+const int B = 4275 ;
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println("test");
+  temp = analogRead(TEMP_SENSOR_PIN) ;
+  float R = 1023.0/((float) temp) - 1.0 ;
+  R = 100000.0*R;
+  float temperature = 1.0/(log(R/100000.0)/B + 1/298.15) - 273.15 ;
+  Serial.print("Temperature : ");
+  Serial.println(temperature) ;
+  digitalWrite(LED_BUILTIN, HIGH) ;
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW) ;
+  delay(1000);
 }
