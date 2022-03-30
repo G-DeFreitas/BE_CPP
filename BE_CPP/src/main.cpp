8c41ec8646a53669f0f67d0badeb170277590483
I-Badier <isaure.badier@gmail.com>
2022-03-30 16:35:58 +0200

    LED AUX

--- BE_CPP/src/main.cpp
+++ BE_CPP/src/main.cpp
@@ -3,14 +3,15 @@ void setup()
 {
   // put your setup code here, to run once:
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_BUILTIN_AUX, OUTPUT);
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
-  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(LED_BUILTIN_AUX, HIGH);
   delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(LED_BUILTIN_AUX, LOW);
   delay(1000);
 
 }
