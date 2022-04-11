4686891c66295a2577975376e65bdf10733e7027
Guillaume De Freitas <81011333+G-DeFreitas@users.noreply.github.com>
2022-04-11 17:54:16 +0200

    Creation Oled

--- ProjetBE/include/EcranOLED.h
+++ ProjetBE/include/EcranOLED.h
@@ -2,26 +2,14 @@
 #define _ECRANOLED_H_
 
 #include "Actionneur.h"
+#include "SeeedOLED.h"
 
 class EcranOLED : public Actionneur
 {
 private:
-    char pinSDA;
-    char pinSCL;
+    SeeedOLED ecran;
+
 public:
-    EcranOLED(char pinSDA, char pinSCL);
 };
-
-
-EcranOLED::EcranOLED(char pinSDA, char pinSCL)
-{
-    this->pinSCL = pinSCL;
-    this->pinSDA = pinSDA;
-
-    // U8G2_SSD1306_128X64_ALT0_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE); // SSD1306 and SSD1308Z are compatible
-}
-
-
-
 
 #endif
